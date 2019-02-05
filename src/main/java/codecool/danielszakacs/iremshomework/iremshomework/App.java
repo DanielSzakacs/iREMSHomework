edf1ce7c549a36162df6d8ebea3b74f65a15543d
danielszakacs <daniel.sokach@gmail.com>
2019-02-05 10:16:19 +0100

    main packages created

---
+++ src/main/java/codecool/danielszakacs/iremshomework/iremshomework/App.java
@@ -0,0 +1,13 @@
+package codecool.danielszakacs.iremshomework.iremshomework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
