6e9fa10cb430f736727c5ac8fb81a3539b64cbf2
danielszakacs <daniel.sokach@gmail.com>
2019-02-05 17:02:41 +0100

    VendingMachineTest class updated. no new test.

--- src/test/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachineTest/VendingMachineTest.java
+++ src/test/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachineTest/VendingMachineTest.java
@@ -4,27 +4,28 @@ import java.util.List;
 import java.util.Arrays;
 
 import codecool.danielszakacs.iremshomework.iremshomework.vendingMachine.VendingMachine;
+import codecool.danielszakacs.iremshomework.iremshomework.vendingMachine.coinManager.CoinManager;
 import junit.framework.TestCase;
 
 public class VendingMachineTest extends TestCase{
 	
-	private VendingMachine vm = new VendingMachine();
+	private CoinManager cm = new CoinManager();
 	
 	
 	public void VendingMachine_checkCoinValid_returnFalse() {
 		List listOfCoin = (List) Arrays.asList(10, 20, 50, 3);
-		boolean result = this.vm.checkCoinValid(listOfCoin);
+		boolean result = this.cm.checkCoinValid(listOfCoin);
 		assertFalse(result);
 	}
 	
 	
 	public void VendingMachine_countRemainingChange_willbe10() {
-		assertEquals(10, this.vm.countRemainingChange("Coka", 15));
+		assertEquals(10, this.cm.countRemainingChange("Coka", 15));
 	}
 	
 	
 	public void VendingMachine_checkIfCoinEnough_returnTrue() {
-		assertFalse(this.vm.checkIfCoinEnough("Coka", 10));
+		assertFalse(this.cm.checkIfCoinEnough("Coka", 10));
 	}
 	
 
