a3f035578bdc71f5057a94b64ff9732f5bdc62cf
danielszakacs <daniel.sokach@gmail.com>
2019-02-05 13:05:02 +0100

    methods in Product class created, tests are running one by one.

--- src/main/java/codecool/danielszakacs/iremshomework/iremshomework/products/Product.java
+++ src/main/java/codecool/danielszakacs/iremshomework/iremshomework/products/Product.java
@@ -1,5 +1,19 @@
 package codecool.danielszakacs.iremshomework.iremshomework.products;
 
 public class Product {
+	private int price;
+	private String name;
+	
+	public Product(String name, int price) {
+		this.name = name; 
+		this.price = price; 
+	}
 
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
 }
