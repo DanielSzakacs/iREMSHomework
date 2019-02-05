f9b82b23b38d2e1ba305743429db04ce78d3b773
danielszakacs <daniel.sokach@gmail.com>
2019-02-05 13:54:39 +0100

    VendingMachine Test and Method checked.

--- src/main/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachine/VendingMachine.java
+++ src/main/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachine/VendingMachine.java
@@ -1,5 +1,20 @@
 package codecool.danielszakacs.iremshomework.iremshomework.vendingMachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class VendingMachine {
+	List<Integer> listOfValidCoin = Arrays.asList(1, 5, 10, 25); 
+	
+	public boolean checkCoinValid (List<Integer> listOfCoin) {
+        AtomicBoolean result = new AtomicBoolean(true);
+        listOfCoin.stream().forEach(t -> {
+            if(!this.listOfValidCoin.contains(t)) {
+                result.set(false);
+            }
+        });
+        return result.get();
+    }
 
 }
--- src/test/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachineTest/VendingMachineTest.java
+++ src/test/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachineTest/VendingMachineTest.java
@@ -1,5 +1,18 @@
 package codecool.danielszakacs.iremshomework.iremshomework.vendingMachineTest;
 
-public class VendingMachineTest {
+import java.util.List;
+import java.util.Arrays;
+
+import codecool.danielszakacs.iremshomework.iremshomework.vendingMachine.VendingMachine;
+import junit.framework.TestCase;
+
+public class VendingMachineTest extends TestCase{
+	private VendingMachine vm = new VendingMachine();
+	
+	public void VendingMachine_checkCoinValid_returnFalse() {
+		List listOfCoin = (List) Arrays.asList(10, 20, 50, 3);
+		boolean result = this.vm.checkCoinValid(listOfCoin);
+		assertFalse(result);
+	}
 
 }
