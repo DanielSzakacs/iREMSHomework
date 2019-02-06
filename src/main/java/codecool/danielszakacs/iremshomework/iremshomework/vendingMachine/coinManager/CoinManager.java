90c6fa96cca87600765a669d9f7a6db61ae6200e
danielszakacs <daniel.sokach@gmail.com>
2019-02-06 10:42:38 +0100

    unused import deleted

--- src/main/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachine/coinManager/CoinManager.java
+++ src/main/java/codecool/danielszakacs/iremshomework/iremshomework/vendingMachine/coinManager/CoinManager.java
@@ -2,7 +2,6 @@ package codecool.danielszakacs.iremshomework.iremshomework.vendingMachine.coinMa
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
